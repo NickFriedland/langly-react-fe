1acec9505d89a6abbc6fa4631d0a3ee7c0b28bb7
Nick Friedland <nick.friedland@gmail.com>
2019-02-23 11:57:58 -0800

    Adding axios for url delivery from react to express

--- src/ArticleInput.js
+++ src/ArticleInput.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 //import './ArticleInput.css';
 // import styled from 'styled-components';
 
 class ArticleInput extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      url: ''
+    }
   }
 
   handleChange(evt) {
      // runs on every keystroke
     this.setState({
-      fullName: evt.target.value
+      [evt.target.name]: evt.target.value
     });
   }
 
-  render() {
-    return <div className='ArticleInput' />;
+  async handleSubmit(evt) {
+    evt.preventDefault();
+
+    let res = await axios.post('http://localhost:3001/', this.state.url);
+    return res;
+    // console.log('HERE');
+
+    // Mercury.parse(this.state.url).then(result => console.log(result));
   }
 
-  state = {};
+  render() {
+    return <div className='ArticleInput'>
+      <h1>HOMEPAGE FOR LANGLY</h1>
+      <form onSubmit={this.handleSubmit}>
+          <label>URL</label>
+          <input
+            name="url"
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.url}
+          />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  }
 }
-
-ArticleInput.propTypes = {};
-
-ArticleInput.defaultProps = {};
 
 export default ArticleInput;
