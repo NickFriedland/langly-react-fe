5b69227eedbf64829ac3c6316970be45caeeb4eb
Nick Friedland <nick.friedland@gmail.com>
2019-03-18 12:44:11 -0700

    Adding package and first take on highlightable word select

--- src/ReadingLevel.js
+++ src/ReadingLevel.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import WordsList from './WordsList';
+import Highlightable, {ranges, onTextHighlightedCallback, uniqueId, onMouseOverHighlightedWordCallback} from 'highlightable';
+
 //import './ReadingLevel.css';
 // import styled from 'styled-components';
 
@@ -19,11 +21,29 @@ class ReadingLevel extends Component {
   //   // });
   // }
 
+  onMouseOverHighlightedWordCallback = (range) => {
+    alert(range);
+  }
+
+  onTextHighlightedCallback = (range) => {
+    alert(range);
+  }
+
   render() {
+    // let newLine = this.props.text.split('\n').map(t => <p>{t}</p>).toString();
     return <div className='ReadingLevel'> 
       <h1>{this.props.title}</h1>
       <h2>{this.props.readability}</h2>
-      {this.props.text.split('\n').map(t => <p>{t}</p>)}
+      <Highlightable ranges={ranges}
+               enabled={true}
+               onTextHighlighted={onTextHighlightedCallback}
+               id={uniqueId}
+               onMouseOverHighlightedWord={onMouseOverHighlightedWordCallback}
+               highlightStyle={{
+                 backgroundColor: '#ffcc80'
+               }}
+               text={this.props.text}
+/>
       {/* {this.state.words.length ?
         <WordsList words={this.state.words} /> : ''
       } */}
